927793e3aca23d686b696ae6d8291f7d2081f795
Francisco Trejo <informatica@delfos-cloud.com>
2020-09-28 09:56:22 -0600

    [ADD]printer_fiscal, se agrego la opcion para enlazar diario con impresora fiscal donde se realizara la impresion de la factura

--- printer_fiscal/__manifest__.py
+++ printer_fiscal/__manifest__.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 {
     'name': 'Impresion Fiscal Panama',
     'version': '1.0',
@@ -7,8 +8,10 @@
     'author': 'Intelitecsa (Francisco Trejo)',
     'website': 'https://www.intelitecsa.com',
     'license': 'GPL-3',
-    'depends': ['account'],
-    'data': ['views/account_invoice.xml'],
+    'depends': ['base',
+                'account'],
+    'data': ['views/account_invoice.xml',
+             'views/account_journal.xml'],
     # 'demo': [],
     'installable': True,
     'auto_install': False
