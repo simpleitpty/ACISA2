f8a5ddf85bd42eff40d3206d957bef1eb8dc0927
Francisco Trejo <informatica@delfos-cloud.com>
2021-01-04 19:48:19 -0600

    [FIX] printer_fiscal, invoice_refund, se corrige problema con facturas eliminadas

--- printer_fiscal/models/account_invoice.py
+++ printer_fiscal/models/account_invoice.py
@@ -7,19 +7,19 @@ class AccountInvoice(models.Model):
 
     num_doc_fiscal = fields.Char(string='Numero Fiscal')
     is_print = fields.Boolean('Ha sido impresa')
-    number_integer = fields.Char('Numero', compute='numeracion', store=True)
+    number_integer = fields.Char('Numero', compute='_numeracion', store=True)
 
-    @api.one
-    def numeracion(self):
+    @api.depends('number')
+    def _numeracion(self):
         i = 0
         n = "0123456789"
-        num = self.number
-        if num:
-            for letra in num:
-                i += 1
-                if letra not in n:
-                    long = i
-            number = num[long:]
-        else:
-            number = False
-        return number
+        for record in self:
+            num = record.number
+            if num:
+                for letra in num:
+                    i += 1
+                    if letra not in n:
+                        long = i
+                record.number_integer = num[long:]
+            else:
+                record.number_integer = 0
