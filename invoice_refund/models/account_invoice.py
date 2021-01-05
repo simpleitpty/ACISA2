3dbf7ad9c327fcf91b83b5e76793ccad6c8735cb
Francisco Trejo <informatica@delfos-cloud.com>
2021-01-04 18:47:13 -0600

    [FIX] printer_fiscal, invoice_refund, se corrige problema con facturas eliminadas

--- invoice_refund/models/account_invoice.py
+++ invoice_refund/models/account_invoice.py
@@ -1,13 +1,15 @@
 # -*- coding: utf-8 -*-
 from odoo import api, fields, models, _, exceptions
 
+
 class AccountInvoice(models.Model):
     _inherit = 'account.invoice'
-    
-    inv_refund_id = fields.Many2one('account.invoice','Factura Relacionada', copy=False, track_visibility='onchange')
+
+    inv_refund_id = fields.Many2one('account.invoice', 'Factura Relacionada', copy=False, track_visibility='onchange')
 
     state_refund = fields.Selection([
-            ('refund','Retificada'),
-            ('no_refund','No Retificada'),
-        ], string="Retificada", index=True, readonly=True, default='no_refund',
+        ('refund', 'Retificada'),
+        ('no_refund', 'No Retificada'),
+    ], string="Retificada", index=True, readonly=True, default='no_refund',
         track_visibility='onchange', copy=False)
+    inv_refund_number = fields.Char('Numero', related='inv_refund_id.number_integer')
